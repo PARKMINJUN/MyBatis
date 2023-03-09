e82b03b072e59be89b7bfaa81c34d407d6227c44
PARKMINJUN <junee0530@naver.com>
2023-03-09 21:28:31 +0900

    세팅

---
+++ src/main/java/com/example/app_2023_03_09/App20230309Application.java
@@ -0,0 +1,13 @@
+package com.example.app_2023_03_09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20230309Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App20230309Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/app_2023_03_09/App20230309ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app_2023_03_09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App20230309ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
